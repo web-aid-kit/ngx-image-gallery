c52427fdad1b090c744463cf8db44162ae882265
Anthony Nahas <anthony.na@hotmail.de>
2017-11-12 19:30:03 +0100

    Fix: @SSR --> hammerjs should be imported in main.ts of an angular app

--- module/ngx-image-gallery.module.ts
+++ module/ngx-image-gallery.module.ts
@@ -1,7 +1,7 @@
 import { ClickOutsideDirective } from './directives/click-outside.directive';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import 'hammerjs';
+// import 'hammerjs';
 
 import { NgxImageGalleryComponent } from './components/ngx-image-gallery/ngx-image-gallery.component';
 import { MouseWheelDirective } from './directives/mousewheel.directive';
