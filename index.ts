b1c2f1672bb045e8bbd9ff7b1eeda586a249086c
Anthony Nahas <anthony.na@hotmail.de>
2017-11-13 20:36:51 +0100

    Ref: @index

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { GALLERY_CONF, GALLERY_IMAGE } from './src/ngx-image-gallery.conf';
-export { NgxImageGalleryModule } from './src/index';
-export { NgxImageGalleryComponent } from './src/components/ngx-image-gallery/ngx-image-gallery.component';
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import {MouseWheelDirective} from './directives/mousewheel.directive';
 export * from './components/ngx-image-gallery/ngx-image-gallery.component';
 export * from './directives/click-outside.directive';
 export * from './directives/mousewheel.directive';
+export * from './ngx-image-gallery.conf';
 
 
 @NgModule({
