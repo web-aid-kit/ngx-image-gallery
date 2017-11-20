f4f1c6a69ecea0809f823783d3df8de98d056aab
Anthony Nahas <anthony.na@hotmail.de>
2017-11-20 07:02:45 +0100

    Ref: @demo_path files project

--- demo-app/src/app/app.module.ts
+++ demo-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {NgxImageGalleryModule} from "../../../dist/ngx-image-gallery";
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxImageGalleryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
